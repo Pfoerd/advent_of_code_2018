af05e4a7d99ca00934ad088b405b2384f27d250f
fesc7420 <ferdinand.schneider@esolutions.de>
2019-01-17 15:50:40 +0100

    solutions for day 12 part 1 and 2

--- src/day_12/day_12_2.ts
+++ src/day_12/day_12_2.ts
@@ -1,18 +1,19 @@
 import * as os from 'os';
 
 export function day12_part2(rawFileData: string) {
+    /*
+    Strategy: after some (400) initial generations the resulting sum grows with a constant value.
+    So lets just calculate 500 generations, get the average growth after the first 400 generations and then
+    interpolate the sum after the 50 billion generations
+    */
     const inputData: string[] = rawFileData.split(os.EOL);
     let pots: string = inputData.splice(0, 2)[0].substr(15);
+    // we only need to know one side of the rules
     const spreadRules = inputData.filter(v => v.indexOf('=> #') !== -1).map(v => v.substr(0, 5));
 
+    // calculates the sum of indices needed for the result
     const currentSum: () => number = () => {
-        let sum = 0;
-        for (let i = 0; i < pots.length; i++) {
-            if (pots[i] === '#') {
-                sum += (i - zeroPotPosition);
-            }
-        }
-        return sum;
+        return [...pots].reduce((a, v, i) => a + (v === '#' ? i - zeroPotPosition : 0), 0);
     };
 
     const diffs: number[] = [];
@@ -20,7 +21,8 @@ export function day12_part2(rawFileData: string) {
 
     let zeroPotPosition = 0;
     for (let generation = 1; generation <= 500; generation++) {
-        // insert missing pots
+        // insert missing pots. We always want to start with a min of 5 empty
+        // pots at the beginning and at the end to always cover the '.....' rule
         const missingPotsCountBeginning = Math.max(0, 5 - pots.indexOf('#'));
         const missingPotsCountEnd = Math.max(0, 5 - (pots.length - 1 - pots.lastIndexOf('#')));
         zeroPotPosition += missingPotsCountBeginning;
@@ -28,17 +30,18 @@ export function day12_part2(rawFileData: string) {
             + pots
             + Array.from(Array(missingPotsCountEnd), () => '.').reduce((a, v) => a + v, '');
 
-        let newGeneration = '..';
+        let newGeneration = '';
         for (let pot = 2; pot < pots.length - 2; pot++) {
             const nearbyPots = pots.slice(pot - 2, pot + 3);
 
             const newPotValue = spreadRules.indexOf(nearbyPots) !== -1 ? '#' : '.';
             newGeneration += newPotValue;
         }
+        zeroPotPosition -= 2;
         pots = newGeneration;
 
         const sum = currentSum();
-        diffs.push(sum - lastSum);
+        diffs.push(currentSum() - lastSum);
         lastSum = sum;
     }
 
